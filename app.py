1c021c6cce1b53b72fc1b9c189c0c9ba1f7e976a
LUAN GAMA <luan.gama.psicologo@gmail.com>
2025-06-08 21:25:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -586,7 +586,7 @@ if not st.session_state.accepted_terms:
     else:
         st.stop()
 
-st.header("🧠 Psicólogo Assistente 🧠", divider="gray")
+st.header("🧠 Psicólogo Assistente / Elaboração de Documentos 🧠", divider="gray")
 nome = st.text_input("SEU NOME COMPLETO")
 numero = st.text_input("CRP")
 data = st.text_input("DATA")
